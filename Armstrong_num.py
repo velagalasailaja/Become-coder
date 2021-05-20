01bd96adbbab13df31b8a7db9bf77fdad5bbdb53
velagalasailaja <84116001+velagalasailaja@users.noreply.github.com>
2021-05-20 14:39:58 +0530

    Update Armstrong_num.py

--- Armstrong_num.py
+++ Armstrong_num.py
@@ -1,17 +1,19 @@
 num=int(input())
-count=0
-temp1=num
-temp2=num
-while num:
-    num=num//10
-    count+=1
-##print("No of digits=",count)
-res=0
-while temp1:
-    r=temp1%10
-    temp1=temp1//10
-    res=res+pow(r,count)
-if res==temp2:
-    print("ARMSTRONG NUMBER")
-else:
-    print("NOT ARMSTRONG NUMBER")
+def armstrong(num):
+    count=0
+    temp1=num
+    temp2=num
+    while num:
+        num=num//10
+        count+=1
+    ##print("No of digits=",count)
+    res=0
+    while temp1:
+        r=temp1%10
+        temp1=temp1//10
+        res=res+pow(r,count)
+    if res==temp2:
+        print("ARMSTRONG NUMBER")
+    else:
+        print("NOT ARMSTRONG NUMBER")
+armstrong(num)
